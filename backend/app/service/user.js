e7de54cc4695bfab3a8b7f425e5b381fc99c4bee
刘超权 <2857936496@qq.com>
2025-07-13 16:51:22 +0800

    refactor:重构登录、注册接口

--- backend/app/service/user.js
+++ backend/app/service/user.js
@@ -7,6 +7,13 @@ const crypto = require('crypto');
 class UserService extends Service {
   // 注册
   async register(username, password) {
+
+    // 检查用户是否存在
+    const existingUser = await this.ctx.model.User.findOne({ username });
+    if (existingUser) {
+      this.ctx.throw(409, '用户名已存在');
+      return;
+    }
     const user = new this.ctx.model.User();
     const pwd = crypto.createHash('md5')
       .update(password)
@@ -24,17 +31,13 @@ class UserService extends Service {
       .update(password)
       .digest('hex');
     const user = await ctx.model.User.findOne({ username, password: pwd });
+    if (!user) {
+      ctx.throw(401, '用户名不存在');
+    }
+    if (user.password !== pwd) {
+      ctx.throw(401, '密码错误');
+    }
     return user;
-    // const user = await ctx.model.User.findOne({ username });
-    // if (!user) {
-    //   ctx.throw(401, '用户名不存在');
-    // }
-    // const pwd = crypto.createHash('md5')
-    //   .update(password)
-    //   .digest('hex');
-    // if (user.password !== pwd) {
-    //   ctx.throw(401, '密码错误');
-    // }
   }
 }
 
